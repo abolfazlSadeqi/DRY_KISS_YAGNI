5ebc66f3bd0db1cb7ef947ff229e5a27073b82c2
abolfazlsadeqi <abolfazlsadeqi@gmail.com>
2024-02-25 19:58:53 +0330

    add  UserManagement example to YAGNI

---
+++ YAGNI/YAGNI/NonYAGNI/UserManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGNI.NonYAGNI;
+
+public class UserManagement
+{
+    public bool CheckUser(string username,string Password)
+    {
+        //to do
+
+        return true;
+    }
+
+    // Unnecessary feature 
+    public bool CheckUserByName(string username,string Firstname,string Family, string Password)
+    {
+        // to do
+
+        // Unnecessary feature 
+
+        return true;
+    }
+}
---
+++ YAGNI/YAGNI/WithYAGNI/UserManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGNI.WithYAGNI;
+
+public class UserManagement
+{
+    public bool CheckUser(string username,string Password)
+    {
+        //to do
+
+        return true;
+    }
+
+}
